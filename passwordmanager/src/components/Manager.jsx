2a5d7e8eb7ebdf6d83b0994b2c440be9ebdc241e
astikadarsh <astikadarsh7061@gmail.com>
2026-01-25 22:02:57 +0530

    fourth commit

--- passwordmanager/src/components/Manager.jsx
+++ passwordmanager/src/components/Manager.jsx
@@ -2,21 +2,33 @@ import React from 'react'
 
 const Manager = () => {
     return (
-<>
-        <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
+        <>
+            <div class="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
 
-        <div className="container mx-auto bg-slate-50 max-w-4xl px-40 py-16">
-            <h1>PassVault</h1>
-            <p>A Secure Password Mangement Tool</p>
-        <div className="text-white flex flex-col p-4">
-            <input className='bg-white rounded-full' type="text" />
-            <div className="flex">
-                <input className='bg-white' type="text" />
-                <input className='bg-white' type="text" />
+            <div className="container mx-auto max-w-4xl px-40 py-16">
+                <h1 className='text-4xl font-bold text-center'>
+                    <span className='text-green-500'> &lt;</span>
+                    Pass
+                    <span className='text-green-500'>Vault/ &gt;</span>
+                </h1>
+                <p className='text-green-900 text-lg text-center'>A Secure Password Mangement Tool</p>
+                <div className="text-black flex flex-col p-4 gap-8 items-center">
+                    <input className='bg-white rounded-full border border-green-500 w-full p-4 py-1' type="text" />
+                    <div className="flex w-full justify-between gap-8">
+                        <input className='bg-white rounded-full border border-green-500 w-full p-4 py-1' type="text" />
+                        <input className='bg-white rounded-full border border-green-500 w-full p-4 py-1' type="text" />
+                    </div>
+
+                    <button className='flex justify-center items-center bg-green-400 hover:bg-green-300 rounded-full px-4 py-2 w-fit'>
+                        <lord-icon
+                            src="https://cdn.lordicon.com/vjgknpfx.json"
+                            trigger="hover"
+                            colors="primary:#000000,secondary:#000000">
+                        </lord-icon>
+                        Add Password</button>
+                </div>
             </div>
-        </div>
-        </div>
-</>
+        </>
     )
 }
 
--- passwordmanager/src/components/Navbar.jsx
+++ passwordmanager/src/components/Navbar.jsx
@@ -5,10 +5,10 @@ const Navbar = () => {
     <nav className='bg-slate-800 text-white'>
       <div className="container mx-auto flex justify-between items-center px-4 py-5 h-14">
 
-      <div className="logo font-bold text-white">
-        <span className='text-green-700'> &lt;</span>
-        PassVault
-        <span className='text-green-700'>/ &gt;</span>
+      <div className="logo font-bold text-white text-2xl">
+        <span className='text-green-500'> &lt;</span>
+        Pass
+        <span className='text-green-500'>Vault/ &gt;</span>
         
 
       </div>
