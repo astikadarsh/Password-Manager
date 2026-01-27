5c86a25dae34ae6769ca73e8e36a844f9bf34b7f
astikadarsh <astikadarsh7061@gmail.com>
2026-01-27 15:18:35 +0530

    sixth commit

--- passwordmanager/src/components/Manager.jsx
+++ passwordmanager/src/components/Manager.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { useRef } from 'react';
 const Manager = () => {
+    const ref = useRef()
+    const showPassword = () => {
+        alert("showing password");
+        if (ref.current.src.includes("icons/eyecross.png")) {
+
+            ref.current.src = "icons/eye.png"
+        }
+        else {
+            ref.current.src = "icons/eyecross.png"
+        }
+    }
+
     return (
         <>
-            <div class="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
+            <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div></div>
 
             <div className="container mx-auto max-w-4xl px-40 py-16">
                 <h1 className='text-4xl font-bold text-center'>
@@ -18,8 +30,8 @@ const Manager = () => {
                         <input placeholder='enter username' className='bg-white rounded-full border border-green-500 w-full p-4 py-1' type="text" />
                         <div className="relative">
                             <input placeholder='enter password' className='bg-white rounded-full border border-green-500 w-full p-4 py-1' type="text" />
-                            <span className='absolute right-0 top-1'>
-                                <img src="icons/eye.png" alt="eye" />
+                            <span className='absolute right-[-2px] top-[7px] cursor-pointer' onClick={showPassword}>
+                                <img ref={ref} className='p-1' width={20} src="icons/eye.png" alt="eye" />
                             </span>
                         </div>
                     </div>
